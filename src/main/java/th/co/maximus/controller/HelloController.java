d208806015f619eda0d080f947c45b9e34f126a3
Xunxas <a.e_anusorn@hotmail.com>
2018-03-09 00:51:12 +0700

    commit

--- src/main/java/th/co/maximus/controller/HelloController.java
+++ src/main/java/th/co/maximus/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import th.co.maximus.bean.Employee;
 import th.co.maximus.service.HelloService;
@@ -21,6 +24,17 @@ public class HelloController {
 		model.addAttribute("allEmployees", new ArrayList<Employee>());
 //		(ArrayList<Employee>)employeeServiceInterface.getAllEmployees()
 		return "index";
+	}
+	
+	@PostMapping("/insert/save")
+	public String save(@ModelAttribute("employee") Employee employee, final RedirectAttributes redirectAttributes) {
+		if(service.insert(employee)) {
+			redirectAttributes.addFlashAttribute("saveEmployee", "success");
+		} else {
+			redirectAttributes.addFlashAttribute("saveEmployee", "unsuccess");
+		}
+		
+		return "redirect:/";
 	}
 
 }
--- src/main/java/th/co/maximus/service/HelloService.java
+++ src/main/java/th/co/maximus/service/HelloService.java
@@ -1,8 +1,13 @@
 package th.co.maximus.service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import th.co.maximus.bean.Employee;
@@ -16,11 +21,18 @@ private JdbcTemplate jdbcTemplate;
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
-	public void insert(Employee employee) {
+	public boolean insert(Employee employee) {
+		boolean result = true;
 		StringBuilder sql = new StringBuilder();
 		sql.append(" INSERT INTO student ( studentID, firstname, lastname, email, phone, majorID) ");
 		sql.append(" VALUES (?,?) ");
-		jdbcTemplate.update(sql.toString(), employee.getStudentID(), employee.getFirstName(), employee.getLastName(), employee.getEmail(), employee.getPhone(), employee.getMajorID());
+		try {
+			jdbcTemplate.update(sql.toString(), employee.getStudentID(), employee.getFirstName(), employee.getLastName(), employee.getEmail(), employee.getPhone(), employee.getMajorID());
+		}catch(Exception e) {
+			e.printStackTrace();
+			result = false;
+		}
+		return result;
 	}
 	
 	public void update(Employee employee) {
@@ -30,6 +42,42 @@ private JdbcTemplate jdbcTemplate;
 		sql.append(" WHERE studentID = ? ");
 		jdbcTemplate.update(sql.toString(), employee.getFirstName(), employee.getLastName(), employee.getEmail(), employee.getPhone(), employee.getMajorID());
 		
+	}
+	
+	public void delete(int id) {
+		StringBuilder sql = new StringBuilder();
+		sql.append(" DELETE from student ");
+		sql.append(" WHERE studentID = ? ");
+		jdbcTemplate.update(sql.toString(), id);
+	}
+	
+	public List<Employee> findAll() {
+		StringBuilder sql = new StringBuilder();
+		sql.append(" SELECT * FROM student ");
+		return jdbcTemplate.query(sql.toString() , new Mapp());
+	}
+	
+	public Employee findByID(long id) {
+		StringBuilder sql = new StringBuilder();
+		sql.append(" SELECT * FROM student where studentID = ");
+		sql.append(id);
+		return jdbcTemplate.queryForObject(sql.toString() , new Mapp());
+	}
+	
+	private static final class Mapp implements RowMapper<Employee> {
+
+		@Override
+		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Employee employee = new Employee();
+			employee.setStudentID(rs.getString("studentID"));
+			employee.setFirstName(rs.getString("firstname"));
+			employee.setLastName(rs.getString("lastname"));
+			employee.setEmail(rs.getString("email"));
+			employee.setPhone(rs.getString("phone"));
+			employee.setMajorID(rs.getInt("majorID"));
+			return employee;
+		}
+
 	}
 	
 }
